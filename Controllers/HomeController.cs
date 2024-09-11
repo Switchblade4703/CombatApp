b1906ee643eac5c86cfbe12b75aae434d9ec24bc
Switchblade4703 <156360989+Switchblade4703@users.noreply.github.com>
2024-09-11 13:37:44 -0500

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using CombatApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CombatApp.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var Protag = new Character();
+            Protag.HP = 15;
+            Protag.SkillPoints = 10;
+            ViewBag.Win = true;
+            return View(Protag);
+
+            
+        }
+        [HttpPost]
+        public IActionResult Index(Character Protag)
+        {
+
+            Protag.SkillPoints += 1;
+            Protag.Level += 1;
+
+            var Enemy = new Character();
+
+
+            Enemy.HP = 99;
+            Enemy.Attack = 99;
+            Enemy.Defense = 99;
+
+            if (Protag.Combat(Enemy) == 0)
+            {
+                ViewBag.Win = true;
+            }else
+            {
+                ViewBag.Win = false;
+            }
+            return View(Protag);
+        }
+
+    }
+}
---
+++ Models/Character.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CombatApp.Models
+{
+    public class Character
+    {
+        [Required]
+        public int HP { get; set; } = 1;
+        [Required]
+        public string Name { get; set; } = "Default";
+        [Required]
+        public int Attack { get; set; } = 1;
+        [Required]
+        public int Defense { get; set; } = 1;
+        [Required]
+        public int SkillPoints { get; set; } = 0;
+        [Required]
+        public int Magic { get; set; } = 1;
+        [Required]
+        public int Level { get; set; } = 1;
+
+        public int Combat(Character Enemy)
+        {
+            while (this.HP > 0 && Enemy.HP > 0)
+            {
+                //combatloop
+                this.HP -= Enemy.Attack;
+                Enemy.HP -= this.Attack;
+            }
+
+            if(Enemy.HP <= 0)
+            {
+                return 0;
+            }
+            return 1;
+        }
+    }
+}
